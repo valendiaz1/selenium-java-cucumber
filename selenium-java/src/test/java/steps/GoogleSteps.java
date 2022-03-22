aa69b2a00f6c91bbb057cabbdf1a04b7a074006f
valendiaz1 <diazemi000@gmail.com>
2022-03-22 21:45:26 +0100

    agrego BasePage, GooglePage, y Google.feature

---
+++ selenium-java/src/test/java/steps/GoogleSteps.java
@@ -0,0 +1,32 @@
+package steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.GooglePage;
+
+public class GoogleSteps {
+
+    GooglePage google = new GooglePage();
+
+    @Given("^I am on the Google search page$")
+    public void navigateToGoogle(){
+        google.navigateToGoogle();
+    }
+
+    @When("^I enter a search criteria$")
+    public void enterSearchCriteria(){
+
+    }
+
+    @And("^click on the search button$")
+    public void clickSearchButton(){
+
+    }
+
+    @Then("^The results match the criteria$")
+    public void validateResults(){
+
+    }
+}
